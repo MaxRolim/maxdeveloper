e2ff333c44567c40ab726d0b3f4dbdc153f9ea5f
MaxRolim <maxcarlos656@gmail.com>
2024-11-28 20:39:33 -0300

    pagina_v4..0

---
+++ script.js
@@ -0,0 +1,30 @@
+async function sendMessage() {
+    // Obtem o a mensagem digitada pelo usuario no campo de texto 
+    const message = document.getElementById('message').value.trim();  // Email do cliente
+    const email = document.getElementById('email').value.trim();    //Mensagem do cliente
+    
+    if (!email || !message) { //Alerta se houver campos vazios
+            alert('Por favor preencha todos os campos');
+            return;
+    }
+    // tenta enviar mensagem para o backend 
+    try {
+        const response = await fetch('http://localhost:3000/send-email', {
+        method: 'POST', //Metodo http usado para enviar os dados 
+        headers: {
+            'Content-Type': 'application/json', //formato dos dados enviados
+            },
+            body:JSON.stringify({email, message}), // Converte os valores para JSON
+        
+        });
+        //Verifica se o envio foi bem sucedido
+        if (response.ok) {
+            alert('Mensagem enviada para o email!'); //Alerta de sucesso   
+        } else{
+            alert('Erro ao enviar mensagem ;( '); // Alerta falha ao enviar mensagem
+        }
+    } catch (error) {
+        alert('Erro ao conectar com servidor') // Alerta se o backend não responde
+    }
+    
+}
