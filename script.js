1edbe5ec8cd4ce7ffdf4cac79b1f77988682719f
Max Rolim <65989828+MaxRolim@users.noreply.github.com>
2024-11-29 23:04:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function sendMessage() {
     }
     // tenta enviar mensagem para o backend 
     try {
-        const response = await fetch('https://maxdeveloper.vercel.app/', {
+        const response = await fetch('https://maxdeveloper.vercel.app/', { // aqui é o url que comunica com o backend
         method: 'POST', //Metodo http usado para enviar os dados 
         headers: {
             'Content-Type': 'application/json', //formato dos dados enviados
